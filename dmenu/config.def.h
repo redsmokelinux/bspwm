8eb2b7b9017c5f3f9d2bcb0a56c30325e5240436
redsmokelinux <117936863+redsmokelinux@users.noreply.github.com>
2022-11-15 20:48:36 +0530

    Add files via upload

---
+++ dmenu/config.def.h
@@ -0,0 +1,26 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+/*	"Iosevka Nerd Font Mono:style=Bold:size=15"*/
+"Fira Mono:style=Medium:size=16"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#bbbbbb", "#1e222a" },
+	[SchemeSel] = { "#eeeeee", "#2c3038" },
+	// [SchemeSel] = { "#eeeeee", "#005577" },
+	// [SchemeOut] = { "#000000", "#ffffff" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
